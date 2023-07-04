376cc5182e632dd3fe10ca0b1c3503e024ec2264
Daniel Savage <93429109+saviscoding@users.noreply.github.com>
2023-07-04 12:31:46 +0100

    amplify init

--- src/script.js
+++ src/script.js
@@ -1,18 +1,25 @@
-document.getElementById('uploadForm').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent form submission
+import Amplify, { Storage } from "aws-amplify"
+import awsconfig from "./aws-exports"
 
-  // Get the selected MP3 file
-  var fileInput = document.getElementById('mp3FileInput');
-  var file = fileInput.files[0];
+Amplify.configure(awsconfig)
 
-  // Upload the file to S3 using Amplify
-  Amplify.Storage.put(file.name, file, { level: 'public' })
-    .then(result => {
-      console.log('File uploaded successfully:', result);
-      // Handle success or update UI as needed
-    })
-    .catch(error => {
-      console.error('Error uploading file:', error);
-      // Handle error or display error message
-    });
-});
+document
+	.getElementById("uploadForm")
+	.addEventListener("submit", function (event) {
+		event.preventDefault() // Prevent form submission
+
+		// Get the selected MP3 file
+		var fileInput = document.getElementById("mp3FileInput")
+		var file = fileInput.files[0]
+
+		// Upload the file to S3 using Amplify
+		Amplify.Storage.put(file.name, file, { level: "public" })
+			.then((result) => {
+				console.log("File uploaded successfully:", result)
+				// Handle success or update UI as needed
+			})
+			.catch((error) => {
+				console.error("Error uploading file:", error)
+				// Handle error or display error message
+			})
+	})
