ea82513505eb707e14888e7c4928757bbfeb2e0f
Daniel Savage <93429109+saviscoding@users.noreply.github.com>
2023-07-04 12:06:13 +0100

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "ssmss565a4fa5": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "music": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
